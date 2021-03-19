e9dc356ac39ed77d2d35c1315b4941c97264f207
devcon <devcon@admantium.com>
2021-03-19 21:01:35 +0100

    03 sized integers

---
+++ 03_sized_integers.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char* argv[]) {
+  int8_t i8;
+  int16_t i16;
+  int32_t i32;
+  int64_t i64;
+  
+  printf("Max size of i8 %zu bytes\n", sizeof(i8));
+  printf("Max size of i16 %zu bytes\n", sizeof(i16));
+  printf("Max size of i32 %zu bytes\n", sizeof(i32));
+  printf("Max size of i64 %zu bytes\n", sizeof(i64));
+}
