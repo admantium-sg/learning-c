c71b19b205cc4e764d88405e4078d4ca7987b629
devcon <devcon@admantium.com>
2021-03-19 20:54:53 +0100

    02 type limits

---
+++ 02_type_limits.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  char c = 0;
+  int i = 0;
+  short s = 0;
+  long l = 0;
+
+  printf("Max size of char %zu bytes\n", sizeof(c));
+  printf("Max size of int %zu bytes\n", sizeof(i));
+  printf("Max size of short %zu bytes\n", sizeof(s));
+  printf("Max size of long %zu bytes\n", sizeof(l));
+}
