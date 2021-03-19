768c54b87c10b53e0b5c23588596adfb9eea9a29
devcon <devcon@admantium.com>
2021-03-19 20:48:39 +0100

    01 print msg

---
+++ 01_print_msg.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+  printf("Please insert a msg: ");
+  getchar();
+  printf("\nGoodbye!\n");
+}
