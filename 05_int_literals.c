d4a496af8e8cfd5de04fc32385d44c812aad8b70
devcon <devcon@admantium.com>
2021-03-20 11:02:02 +0100

    05 int literals

---
+++ 05_int_literals.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int i = 10;
+  long int li = 100L;
+  unsigned int ui = 35531U;
+
+  printf("Int i %d\n", i);
+  printf("Int li %Ld\n", li);
+  printf("Int ui %d\n", ui);
+  
+  printf("Int ui Hex %X\n", ui);
+  printf("Int ui Oct %o\n", ui);
+
+  printf("\nGoodbye!\n");
+}
