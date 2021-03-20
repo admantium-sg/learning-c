bd745529485a5007dd080edfa290f597c64731c9
devcon <devcon@admantium.com>
2021-03-20 10:56:12 +0100

    0f floats

---
+++ 04_floats.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  float f, f_exp, f_hex; //precision: 7 digits
+  double d; //precision: 15 digits
+  long double ld; //precision: 15 digits
+
+  f = 427135.1451;
+  f_exp = 42e5;
+  f_hex = 0xc0ffe;
+  d = 325.35125326436347347474326;
+  ld = 325.3512532643634734747432635125326436347347474326L;
+
+  printf("Float f %f\n", f);
+  printf("Float f_exp %f\n", f_exp);
+  printf("Float f_hex %f\n", f_hex);
+  printf("Double d %f\n", d);
+  printf("Double ld %Ld\n", ld);
+
+  printf("\nGoodbye!\n");
+}
