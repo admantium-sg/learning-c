a19ca6a1aa0c0a188d66b4d2754f92723b5bde58
devcon <devcon@admantium.com>
2021-03-25 21:35:28 +0100

    cmock unit test 1 finished

--- tests/ints.test.c
+++ tests/ints.test.c
@@ -14,31 +14,50 @@
 #include <cmocka.h>
 
 const size_t max = 100;
-int* numbers[100];
+int numbers[100] = {0};
 int current_index = 0;
 
 void add(int n) {
   if (current_index == max) return;
-  *numbers[current_index] = n;
+  numbers[current_index++] = n;
 }
 
 void incr(int i) {
   if (current_index < i) return;
-  *numbers[current_index] += 1;
+  numbers[i] += 1;
 }
 
 int get(int i) {
   if (i > max) return 0;
-  return *numbers[i];
+  return numbers[i];
 }
 
-static void test1(void **state) {
-  assert_int_equal(0, 0);
+static void numbers_is_0_initialized(void **state) {
+  assert_int_equal(0, numbers[0]);
+}
+
+static void adding_numbers(void **state) {
+  add(10);
+  add(23);
+  add(42);
+  assert_int_equal(10, get(0));
+  assert_int_equal(23, get(1));
+  assert_int_equal(42, get(2));
+}
+
+static void incr_numbers(void **state) {
+  incr(0);
+  incr(0);
+  incr(2);
+  assert_int_equal(12, get(0));
+  assert_int_equal(43, get(2));
 }
 
 int main(int argc, char* argv[]) {
   const struct CMUnitTest tests[] = {
-    cmocka_unit_test(test1),
+    cmocka_unit_test(numbers_is_0_initialized),
+    cmocka_unit_test(adding_numbers),
+    cmocka_unit_test(incr_numbers),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
