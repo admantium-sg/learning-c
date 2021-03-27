81b48581011c2fd03e28b8f270e5d5a589a383d2
devcon <devcon@admantium.com>
2021-03-27 13:58:58 +0100

    cmocka simple test

---
+++ tests/simple.test.c
@@ -0,0 +1,25 @@
+/* 
+* ---------------------------------------
+* Copyright (c) Sebastian Günther 2021  |
+*                                       |    
+* devcon@admantium.com                  |    
+*                                       | 
+* SPDX-License-Identifier: BSD-3-Clause | 
+* ---------------------------------------
+*/
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+static void test_integers(void** state) {
+  assert_int_equal(1,1);
+}
+
+int main(int argc, char* argv[]) {
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_integers),
+  };
+
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
