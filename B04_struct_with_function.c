eef98b6c534f392e7d16c0e70830b92cdc54f826
devcon <devcon@admantium.com>
2021-04-05 11:30:05 +0200

    update b04 struct with functions

--- B04_struct_with_function.c
+++ B04_struct_with_function.c
@@ -20,7 +20,7 @@
 typedef struct Book
 {
   int id;
-  char title[30];
+  char *title;
   bool borrowed;
   void (*set_borrowed)(struct Book *, bool);
 } Book;
@@ -30,17 +30,30 @@ void _set_borrowed(struct Book *b, bool status)
   b->borrowed = status;
 }
 
-Book _book_factory(int id, char title[])
+Book _book_factory(int id, char *title)
 {
   Book b;
 
   b.id = id;
-  memcpy(b.title, title, 30);
+  b.title = title;
   b.borrowed = false;
 
   b.set_borrowed = _set_borrowed;
 
   return b;
+}
+
+Book _book_factory2(int id, char *title)
+{
+  return (Book){.id = id, .title = title};
+
+  // b.id = id;
+  // memcpy(b.title, title, 30);
+  // b.borrowed = false;
+
+  // b.set_borrowed = _set_borrowed;
+
+  // return b;
 }
 
 static void test_book_factory(void **state)
@@ -53,6 +66,11 @@ static void test_book_factory(void **state)
 
   b.set_borrowed(&b, true);
   assert_true(b.borrowed);
+
+  Book b2 = _book_factory2(42, "Programming C");
+  assert_int_equal(42, b2.id);
+  printf("Title 2 %s", b2.title);
+  // assert_memory_equal(&b2.title, &"Programming C", 30);
 }
 
 int main(int argc, char *argv[])
